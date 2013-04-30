3675f19c05771f27785b2c8e726d9740600dfb50
Amit Sengupta <amit.sengupta@learner.manipal.edu>
2013-05-01 04:55:24 +0530

    starting

---
+++ _php/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, initial-scale=1.0, maximum-scale=1">
+    <meta name="description" content="Sequence Game">
+    <meta name="author" content="Amit Sengupta">
+    <meta name="keywords" content="Sequence, Mathematics">
+
+    <link href="_script/_css/modern.css" rel="stylesheet">
+    <link href="_script/_css/modern-responsive.css" rel="stylesheet">
+    <link href="_script/_css/site.css" rel="stylesheet" type="text/css">
+    <link href="_script/_js/google-code-prettify/prettify.css" rel="stylesheet" type="text/css">
+
+    <script type="text/javascript" src="_script/_js/assets/jquery-1.9.0.min.js"></script>
+    <script type="text/javascript" src="_script/_js/assets/jquery.mousewheel.min.js"></script>
+    <script type="text/javascript" src="_script/_js/assets/moment.js"></script>
+    <script type="text/javascript" src="_script/_js/assets/moment_langs.js"></script>
+
+    <script type="text/javascript" src="_script/_js/modern/dropdown.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/accordion.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/buttonset.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/carousel.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/input-control.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/pagecontrol.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/rating.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/slider.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/tile-slider.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/tile-drag.js"></script>
+    <script type="text/javascript" src="_script/_js/modern/calendar.js"></script>
+
+    <title>5equence</title>
+</head>
+<body class="metrouicss" onload="prettyPrint()">
+
+<?php include("navigation.php")?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("_php/header.php")?>
+
+<div class="page" id="page-index">
+    
+</div>
+
+<?php include("_php/footer.php")?>
